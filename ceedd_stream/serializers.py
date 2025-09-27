68bb3bb679d03d3299c592549795887bf156ae4a
Victor Kadiata <vickadiata@gmail.com>
2025-09-27 22:08:48 +0100

    Add serializers and viewset for ZoneContributive model

--- ceedd_stream/serializers.py
+++ ceedd_stream/serializers.py
@@ -1,2 +1,10 @@
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
+
+from .models import ZoneContributive, Bailleur, TypeInfrastructure, Client, Infrastructure, Finance, Inspection, Photo
+
+class ZoneContributiveSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = ZoneContributive
+        geo_field = "geom"
+        fields = '__all__'
--- ceedd_stream/views.py
+++ ceedd_stream/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import ZoneContributive, Bailleur, TypeInfrastructure, Client, Infrastructure, Finance, Inspection, Photo
+from .serializers import ZoneContributiveSerializer
+from rest_framework import viewsets
+
 
 # Create your views here.
+class ZoneContributiveViewSet(viewsets.ModelViewSet):
+    queryset = ZoneContributive.objects.all()
+    serializer_class = ZoneContributiveSerializer
+    #filterset_fields = ['nom', 'description']
+    #search_fields = ['nom', 'description']
+    #ordering_fields = ['nom', 'created_at']
+    ordering = ['nom']
