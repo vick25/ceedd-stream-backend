6ff63974a7dfaed1be6c75980cefd197d5ae1939
Victor Kadiata <vickadiata@gmail.com>
2025-09-28 23:15:01 +0100

    Register ZoneContributive model in admin with custom display and search options

--- ceedd_stream/admin.py
+++ ceedd_stream/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ZoneContributive, Bailleur, TypeInfrastructure, Client, Infrastructure, Finance, Inspection, Photo
 
 # Register your models here.
+@admin.register(ZoneContributive)
+class ZoneContributiveAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'description', 'created_at', 'updated_at')
+    search_fields = ('nom', 'description')
+    ordering = ('nom',)
