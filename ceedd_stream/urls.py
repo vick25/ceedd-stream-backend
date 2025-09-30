d0f6bbe6664e25d19cce189ede16b93f8bb9b09e
Victor Kadiata <vickadiata@gmail.com>
2025-09-30 16:18:42 +0100

    Register additional viewsets for bailleur, typeinfrastructure, client, finance, infrastructure, inspection, and photo in the router

--- ceedd_stream/urls.py
+++ ceedd_stream/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 router = DefaultRouter()
 router.register(r'zonecontributive', views.ZoneContributiveViewSet, basename='zonecontributive')
+router.register(r'bailleur', views.BailleurViewSet, basename='bailleur')
+router.register(r'typeinfrastructure', views.TypeInfrastructureViewSet, basename='typeinfrastructure')
+router.register(r'client', views.ClientViewSet, basename='client')
+router.register(r'finance', views.FinanceViewSet, basename='finance')
+router.register(r'infrastructure', views.InfrastructureViewSet, basename='infrastructure')
+router.register(r'inspection', views.InspectionViewSet, basename='inspection')
+router.register(r'photo', views.PhotoViewSet, basename='photo')
+#router.register(r'role', views.RoleViewSet, basename='role')
+#router.register(r'utilisateur', views.UtilisateurViewSet, basename='utilisateur')
 
 urlpatterns = [
     path('', include(router.urls)),
